d4892860f2a9d3fc31b1e9561c6b7b7d5690d2a8
zhangweixu <zhangweixu@zhuanzhaun.com>
2021-01-08 13:58:15 +0800

    变量

---
+++ test/src/main/java/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        // type typename 数据类型  变量名 作用域
+        int a=2;
+        String name="测试";
+        char x='X';
+        double pi=3.14;
+        // 类变量  static
+        //实例变量
+        //局部变量
+    }
+}
