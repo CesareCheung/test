570463c0aa2a6d18123acc63c6bcfe4f188b369e
zhangweixu <zhangweixu@zhuanzhaun.com>
2021-01-08 10:35:20 +0800

    布尔值

--- test/src/main/java/Demo3.java
+++ test/src/main/java/Demo3.java
@@ -5,8 +5,18 @@ public class Demo3 {
         double b=i;
         // 强制转换 （类型）变量   高---低
         // 自动转换  低---高
-
         System.out.println(i);
         System.out.println(b);
+       /*
+
+       注意点：
+       1、不能对布尔值进行转换
+       2、不能把对象类型转换成不相干类型
+       3、把高容量转换成低容量时，强制转换
+       4、转换时可能存在内存溢出，或精度问题
+
+       */
+        System.out.println((int)23.7);    //23
+        System.out.println((int)-45.93f); //-45
     }
 }
