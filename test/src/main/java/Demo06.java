cd0126e3f446f3e7ba84cb1cc511c6b054bdb900
zhangweixu <zhangweixu@zhuanzhaun.com>
2021-01-08 14:56:36 +0800

    常量

---
+++ test/src/main/java/Demo06.java
@@ -0,0 +1,11 @@
+public class Demo06 {
+    //常量 static final 名称=值 常量一般用大写，修饰符不存在先后顺序
+    static final double PI=3.14;
+
+    public static void main(String[] args) {
+        System.out.println(PI);
+
+
+
+    }
+}
