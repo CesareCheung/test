d611d7c7fd758237a04821df860be4e3702d6c74
zhangweixu <zhangweixu@zhuanzhaun.com>
2021-01-08 10:01:20 +0800

    测试

--- test/src/main/java/Demo3.java
+++ test/src/main/java/Demo3.java
@@ -1,2 +1,12 @@
 public class Demo3 {
+    public static void main(String[] args) {
+        int i=128;
+        //byte b=(byte)i; // 内存溢出
+        double b=i;
+        // 强制转换 （类型）变量   高---低
+        // 自动转换  低---高
+
+        System.out.println(i);
+        System.out.println(b);
+    }
 }
--- test/src/test/java/Hello.java
+++ test/src/test/java/Hello.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Hello {
-}
+/**
+ * @author Cesare
+*/
+
+ public class Hello{
+    public static void main(String[] args){
+        //单行注释
+        /*
+        多行注释
+        */
+        /**
+          *文档注释
+         */
+        System.out.println("Hello World");
+    }
+ }
