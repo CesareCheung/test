32cdd07a6c795d9c69524a006976efc6cd62b450
zhangweixu <zhangweixu@zhuanzhaun.com>
2021-01-11 20:49:53 +0800

    scanner使用

---
+++ test/src/main/java/com/zhang/Demo/base/scanner/Demo02.java
@@ -0,0 +1,19 @@
+package com.zhang.Demo.base.scanner;
+
+
+import java.util.Scanner;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("使用nextline方式接收");
+        if (scanner.hasNextLine()){
+            String str=scanner.nextLine();
+            System.out.println("输出内容："+str);
+        }
+        scanner.close();
+    }
+
+
+
+}
