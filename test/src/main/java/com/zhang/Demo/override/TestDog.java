e3dac11e141e6e85da0648dc1960fe912e19d6f5
zhangweixu <1689719986@qq.com>
2021-01-15 15:02:24 +0800

    override重写

---
+++ test/src/main/java/com/zhang/Demo/override/TestDog.java
@@ -0,0 +1,21 @@
+package com.zhang.Demo.override;
+
+class Animal{
+    public void move(){
+        System.out.println("动物可以跑");
+    }
+}
+
+class Dog extends Animal{
+    public void move(){
+        System.out.println("狗可以跑和走");
+    }
+}
+public class TestDog {
+    public static void main(String[] args) {
+        Animal a=new Animal();
+        Animal b=new Dog();
+        a.move();
+        b.move();
+    }
+}
