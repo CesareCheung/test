b207e3c5d563a02701f68ae4533b2336cf801c3a
zhangweixu <1689719986@qq.com>
2021-01-15 15:03:22 +0800

    顺序结构

---
+++ test/src/main/java/com/zhang/Demo/base/sequence/SequenceDemo.java
@@ -0,0 +1,9 @@
+package com.zhang.Demo.base.sequence;
+
+public class SequenceDemo {
+    public static void main(String[] args) {
+        // 顺序结构，从上到下
+        System.out.println("hello1");
+        System.out.println("hello2");
+    }
+}
