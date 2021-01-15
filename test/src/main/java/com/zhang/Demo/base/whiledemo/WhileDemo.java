7fea2b35d829fe6b0d2675f29f6e41c0c000e297
zhangweixu <1689719986@qq.com>
2021-01-15 15:42:37 +0800

    while(布尔表达式){循环内容}

---
+++ test/src/main/java/com/zhang/Demo/base/whiledemo/WhileDemo.java
@@ -0,0 +1,21 @@
+package com.zhang.Demo.base.whiledemo;
+
+/**
+ while(布尔表达式){循环内容}
+ 为true时一直执行
+
+ */
+public class WhileDemo {
+
+    public static void main(String[] args) {
+        //输出0-100
+        int i=0;
+        while (i<100){
+            i++;
+            System.out.println(i);
+        }
+
+    }
+
+
+}
